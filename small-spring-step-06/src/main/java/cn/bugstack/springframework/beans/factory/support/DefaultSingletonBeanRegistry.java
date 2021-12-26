2d6ba494ecdae097003c2947192d36adfedc54d4
huohuo <1047984367@qq.com>
2021-12-26 13:34:59 +0800

    初始化

---
+++ small-spring-step-06/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -0,0 +1,24 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.factory.config.SingletonBeanRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 默认单例 bean注册实现类
+ */
+public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
+
+    private Map<String, Object> singletonObjects = new HashMap<>();
+
+    @Override
+    public Object getSingleton(String beanName) {
+        return singletonObjects.get(beanName);
+    }
+
+    protected void addSingleton(String beanName, Object singletonObject) {
+        singletonObjects.put(beanName, singletonObject);
+    }
+
+}
