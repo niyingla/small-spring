1338777eb3c939cfafce1b079c65de26c857d862
huohuo <as1314520>
2022-02-09 11:33:30 +0800

    优化aop

---
+++ small-spring-step-17/src/main/java/cn/bugstack/springframework/aop/framework/adapter/MethodAroundInterceptor.java
@@ -0,0 +1,39 @@
+package cn.bugstack.springframework.aop.framework.adapter;
+
+import cn.bugstack.springframework.aop.MethodAroundAdvice;
+import cn.bugstack.springframework.aop.MethodBeforeAdvice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * Interceptor to wrap am {@link MethodBeforeAdvice}.
+ * Used internally by the AOP framework; application developers should not need
+ * to use this class directly.
+ */
+public class MethodAroundInterceptor implements MethodInterceptor {
+
+    private MethodAroundAdvice advice;
+
+    public MethodAroundInterceptor() {
+    }
+
+    public MethodAroundInterceptor(MethodAroundAdvice advice) {
+        this.advice = advice;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        this.advice.before(methodInvocation.getMethod(), methodInvocation.getArguments(), methodInvocation.getThis());
+        Object proceed = methodInvocation.proceed();
+        this.advice.after(methodInvocation.getMethod(), methodInvocation.getArguments(), methodInvocation.getThis());
+        return proceed;
+    }
+
+    public MethodAroundAdvice getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(MethodAroundAdvice advice) {
+        this.advice = advice;
+    }
+}
---
+++ small-spring-step-17/src/test/java/cn/bugstack/springframework/test/bean/UserServiceAroundAdvice.java
@@ -0,0 +1,23 @@
+package cn.bugstack.springframework.test.bean;
+
+import cn.bugstack.springframework.aop.MethodAroundAdvice;
+import cn.bugstack.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Component("aroundAdvice")
+public class UserServiceAroundAdvice implements MethodAroundAdvice {
+
+    @Override
+    public void after(Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("环绕后置拦截方法：" + method.getName());
+
+    }
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("环绕前置拦截方法：" + method.getName());
+    }
+
+
+}
